033e19a63fa72afa0725c5bee8f963e0070f68d6
John Cevallos <joancemac@gmail.com>
2021-05-25 09:30:37 -0500

    callback y promesa

---
+++ semana2/promesas.js
@@ -0,0 +1,58 @@
+const libros =[
+    {
+        id:1,
+        titulo:'Sistemas distribuidos con Node.JS',
+        idautor:1
+    },
+    {
+        id:2,
+        titulo:'Aprendiendo JavaScript',
+        idautor:1
+    },
+    {
+        id:3,
+        titulo:'Clean Code JS',
+        idautor:2
+    },
+]
+const autores = [
+    {
+        id:1,
+        nombre:'Miguel Angel'
+    },
+    {
+        id:2,
+        nombre:'Pedro Miguel'
+    },
+    {
+        id:3,
+        nombre:'Julio Verne'
+    }
+]
+
+function buscarLibroPorId(id)
+{
+    return new Promise((resolve, reject)=>{
+        const libro = libros.find((libro)=> libro.id===id);
+        if (!libro)
+        {
+            const error= new Error();
+            error.message="Libro no encontrado";
+            reject(error);
+        }
+        resolve(libro);
+    })
+}
+function buscarAutorPorId(id)
+{
+    return new Promise((resolve,reject)=>{
+        const autor =  autores.find((autor)=> autor.id===id)
+        if (!autor)
+        {
+            const error= new Error();
+            error.message="Autor no encontrado";
+            reject(error);
+        }
+        resolve(autor);
+    })
+}
