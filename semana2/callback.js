3769c00b2161a99f950dc6c01f66c53136066976
John Cevallos <joancemac@gmail.com>
2021-05-25 09:31:01 -0500

    callback y promesa

--- semana2/callback.js
+++ semana2/callback.js
@@ -2,28 +2,73 @@ const libros =[
     {
         id:1,
         titulo:'Sistemas distribuidos con Node.JS',
+        idautor:1
     },
     {
         id:2,
         titulo:'Aprendiendo JavaScript',
+        idautor:1
     },
     {
         id:3,
         titulo:'Clean Code JS',
+        idautor:2
     },
+]
+const autores = [
+    {
+        id:1,
+        nombre:'Miguel Angel'
+    },
+    {
+        id:2,
+        nombre:'Pedro Miguel'
+    },
+    {
+        id:3,
+        nombre:'Julio Verne'
+    }
 ]
 function buscarLibroPorId(id, callback){
     const libro= libros.find((libro)=>  libro.id===id);
     if (!libro)
     {
-        //generar una excepcion
-        //retornar ese error con el callback
+        const error = new Error();
+        error.message="Libro no encontrado";
+        callback(error);
     }
-    //invocar al callback con el libro encontrado
+    callback(null, libro);
+}
+function buscarAutorPorId(id, callback){
+    const autor = autores.find((autor)=>autor.id===id);
+    if (!autor)
+    {
+        const error =  new Error();
+        error.message="Autor no encontrado";
+        return callback(error);
+    }
+    callback(null, autor);
 }
 
-buscarLibroPorId(25, (err, libro )=>{
 
-});
+
+buscarLibroPorId(3,
+(err, libro )=>{
+    if (err)
+    {
+        return console.log(err.message);
+    }
+    buscarAutorPorId(libro.idautor , (err,autor)=>{
+        if (err)
+        {
+            return console.log(err.message);
+        }
+        libro.autor= autor;
+        delete libro.idautor;
+        console.log(libro);
+    })
+}
+);
+
 
 
